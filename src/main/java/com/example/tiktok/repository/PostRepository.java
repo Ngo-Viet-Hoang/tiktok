378c2dcd2e7b18ed184223129b893a68a1965ef8
viethoang <ngoviethoang007@gmail .com>
2022-09-23 10:39:20 +0700

    Update

--- src/main/java/com/example/tiktok/repository/PostRepository.java
+++ src/main/java/com/example/tiktok/repository/PostRepository.java
@@ -4,11 +4,14 @@ import com.example.tiktok.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post,Long> {
-//  Page<Post> findAll(Pageable pageable);
+
+ @Query("SELECT e FROM Post e ORDER BY e.createdAt DESC ")
+ List<Post> findAll();
 }
